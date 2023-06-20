094ca17915c76267399ff25a7da08c16934c5e01
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-20 13:34:50 +0530

    Initial Commit

---
+++ src/EnquiryForm.js
@@ -0,0 +1,13 @@
+import "./EnquiryForm.css";
+
+function EnquiryForm() {
+      return(
+        <div className="header">
+            <h1>ENQUIRY FORM</h1>
+            <h4>If you have any queries kindly take a moment to fill up this form,Our representatives will contact you shortly.</h4>
+        </div>
+      )
+
+}
+
+export default EnquiryForm;
