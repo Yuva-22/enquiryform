d70e46ce768d62b6f55fb56056379892d0da0e36
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-20 13:53:32 +0530

    Input types are added

--- src/EnquiryForm.js
+++ src/EnquiryForm.js
@@ -2,9 +2,21 @@ import "./EnquiryForm.css";
 
 function EnquiryForm() {
       return(
-        <div className="header">
-            <h1>ENQUIRY FORM</h1>
-            <h4>If you have any queries kindly take a moment to fill up this form,Our representatives will contact you shortly.</h4>
+        <div className="enquiry">
+            <div className="head">
+              <h1>ENQUIRY FORM</h1>
+              <h4>If you have any queries kindly take a moment
+                 to fill up this form.</h4>
+              <h4>Our representatives will contact you shortly.</h4>
+            </div>
+            <div className="form-container">
+              <form>
+                  <h1>Name</h1><input placeholder="Name"/>
+                <input placeholder="Email"/>
+                <input placeholder="Subject"/>
+              </form>
+            </div>
+
         </div>
       )
 
