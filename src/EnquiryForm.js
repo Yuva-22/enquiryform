ad06f9799bb57d9a83f5c4eb7bcf788549045eb6
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-21 12:09:44 +0530

    usestate are added

--- src/EnquiryForm.js
+++ src/EnquiryForm.js
@@ -1,6 +1,20 @@
 import "./EnquiryForm.css";
+import React from "react";
+import { useState } from "react";
 
-function EnquiryForm() {
+const EnquiryForm = () => {
+      const [data,setData] = useState({name:"",email:"",bill:"",phone:"",query:""});
+      const handleChange = (e) => {
+           const name = e.target.name;
+           const value = e.target.value;
+           setData({...data,[name]:value})
+      }
+      const handleSubmit = (e) => {
+          e.preventDefault()
+          alert(data)
+
+
+      }
       return(
         <div className="enquiry">
             <div className="head">
@@ -10,16 +24,28 @@ function EnquiryForm() {
               <h4>Our representatives will contact you shortly.</h4>
             </div>
             <div className="form-container">
-              <form>
-                  <h3>Name :</h3><input type="text" placeholder="Customer Name"/>
-                  <h3>E-mail :</h3><input type="email" placeholder="abc@gmail.com"/>
-                  <h3>Bill Number :</h3><input placeholder="Bill Number"/>
-                  <h3>Phone Number :</h3><input type="phone" placeholder="Phone no."/>
-                  <h3>Customer Enquiry :</h3><textarea placeholder="Your Queries..." rows="10"></textarea>
+              <form method="post" onSubmit={handleSubmit}>
+                  <h3>Name :</h3>
+                  <input type="text" name="name" onChange={handleChange} value={data.name} placeholder="Customer Name"/>
+
+                  <h3>E-mail :</h3>
+                  <input type="email" name="email" onChange={handleChange} value={data.email} placeholder="abc@gmail.com"/>
+
+                  <h3>Bill Number :</h3>
+                  <input name="bill" onChange={handleChange} value={data.bill} placeholder="Bill Number"/>
+
+                  <h3>Phone Number :</h3>
+                  <input type="phone" name="phone" onChange={handleChange} value={data.phone} placeholder="Phone no."/>
+
+                  <h3>Customer Enquiry :</h3>
+                  <textarea name="query" placeholder="Your Queries..." onChange={handleChange} value={data.query} rows="10"></textarea>
+
+
                   <div className="btnclass">
-                    <button>Submit</button>
-                    <button>Reset</button>
+                    <button className="submit">Submit</button>
+                    <button className="reset">Reset</button>
                   </div>
+                  
               </form>
             </div>
 
